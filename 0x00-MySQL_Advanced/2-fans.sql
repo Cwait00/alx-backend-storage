63b9410061f61ed42fafa7baddf00aa0caff64ab
Cwait00 <cwaitanobongoza@gmail.com>
2024-05-11 13:09:03 +0000

    -a

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,9 +1,5 @@
 -- Rank country origins of bands by the number of (non-unique) fans
-SELECT origin, SUM(nb_fans) as total_fans
-FROM (
-    SELECT origin, COUNT(*) as nb_fans
-    FROM metal_bands
-    GROUP BY origin
-) AS subquery
+SELECT origin, COUNT(*) as nb_fans
+FROM metal_bands
 GROUP BY origin
-ORDER BY total_fans DESC;
+ORDER BY nb_fans DESC;
