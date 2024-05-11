6b7cdcbe6729a47aa115677fdad930ca365f0a69
Cwait00 <cwaitanobongoza@gmail.com>
2024-05-11 16:44:34 +0000

    -a

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,11 +1,11 @@
--- Create a stored procedure to compute and store the average score for a student
 DELIMITER //
+
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
     -- Declare variables
-    DECLARE total_score INT;
+    DECLARE total_score DECIMAL(10, 2);
     DECLARE num_corrections INT;
-    DECLARE average_score FLOAT;
+    DECLARE average_score DECIMAL(10, 2);
     
     -- Calculate total score and number of corrections
     SELECT SUM(score), COUNT(*) INTO total_score, num_corrections
@@ -24,4 +24,5 @@ BEGIN
     SET average_score = average_score
     WHERE id = user_id;
 END //
+
 DELIMITER ;
