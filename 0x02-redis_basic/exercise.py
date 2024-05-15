f611aad83ac751227952b2ee5fc1e2a04290f1bf
cwait00 <cwaitanobongoza@gmail.com>
2024-05-15 12:38:16 +0000

    exercise.py

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Cache module
+"""
+
+
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """
+    Cache class to interact with Redis
+    """
+
+    def __init__(self):
+        """
+        Initialize Cache instance
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Store data in Redis and return the key
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
+
+
+if __name__ == "__main__":
+    cache = Cache()
+    data = b"hello"
+    key = cache.store(data)
+    print(key)
+
+    local_redis = redis.Redis()
+    print(local_redis.get(key))
