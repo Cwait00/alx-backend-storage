25acd0c444d02fda48ccade46b115ada8b3e8204
cwait00 <cwaitanobongoza@gmail.com>
2024-05-16 17:06:42 +0000

    -a

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import redis
 import uuid
-from typing import Union
+from typing import Any, Callable, Optional, Union
 
 
 class Cache:
@@ -23,3 +23,42 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable[[bytes], Any]] = None) -> Any:
+        """
+        Retrieve data from Redis and apply optional conversion function.
+
+        :param key: Key associated with the stored data.
+        :type key: str
+        :param fn: Optional conversion function to apply on the retrieved data.
+        :type fn: callable, optional
+        :return: Retrieved data, optionally converted using the
+        provided function.
+        :rtype: Any
+        """
+        data = self._redis.get(key)
+        if fn:
+            data = fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        """
+        Retrieve data from Redis and decode it as a UTF-8 string.
+
+        :param key: Key associated with the stored data.
+        :type key: str
+        :return: Retrieved data decoded as a UTF-8 string.
+        :rtype: str
+        """
+        return self.get(key, lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """
+        Retrieve data from Redis and convert it to an integer.
+
+        :param key: Key associated with the stored data.
+        :type key: str
+        :return: Retrieved data converted to an integer.
+        :rtype: int
+        """
+        return self.get(key, int)
